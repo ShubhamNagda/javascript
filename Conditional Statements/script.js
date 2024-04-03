bf460b9884f46c462b00ff2469411de48700a496
Shubham Nagda <119925058+ShubhamNagda@users.noreply.github.com>
2024-04-03 10:45:08 +0530

    Add files via upload

---
+++ Conditional Statements/script.js
@@ -0,0 +1,44 @@
+let age = 16;
+
+if (age >= 18) {
+    console.log("You Can Vote");
+}
+else {
+    console.log("You Can not Vote");
+}
+
+let mode = "dark";
+
+if (mode === "dark") {
+    console.log("dark");
+}
+else {
+    console.log("light")
+}
+
+Aayu = 61;
+
+if (Aayu > 60) {
+    console.log("senior citizen")
+}
+else if (Aayu < 18) {
+    console.log("junior citizen")
+}
+else {
+    console.log("middile");
+}
+
+// Switch
+
+let fruit = "orange";
+
+switch (fruit) {
+    case "orange":
+        console.log("orange");
+        break;
+    case "mango":
+        console.log("mango");
+        break;
+    default:
+        console.log("😊");
+}
---
+++ Conditional Statements/task.js
@@ -0,0 +1,8 @@
+let p = prompt("Enter a number");
+
+if (p % 5 === 0){
+    console.log(p,"is multiple of 5");
+}
+else{
+    console.log(p,"is not a mutiple of 5")
+}
