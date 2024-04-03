fad78561cc714f018e64a9dfd4e5170ced79bf9c
Shubham Nagda <119925058+ShubhamNagda@users.noreply.github.com>
2024-04-03 10:43:54 +0530

    Add files via upload

---
+++ Operators/script.js
@@ -0,0 +1,62 @@
+// Operators -----------------------------------------
+// Arithmetic Operators
+
+// let a = 20;
+// let b = 10;
+// console.log("Arithmetic Operators")
+// console.log("a =", a , "b =", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a ** b = ", a ** b);
+// // unary
+// console.log("++ a = ", ++ a); // prifix
+// console.log("-- b = ", -- b); // prifix
+// console.log("a =",a, "b =",b);
+// console.log("a-- =", a--); //postfix
+// console.log("b++ =",b++); //postfix
+
+// Assignment Operators
+
+// let c = 20;
+// let d = 10;
+
+// console.log("Assignment Operators")
+// console.log("c =",c,"d =",d);
+// console.log("c =",c,"d =",d,"c += d", c +=d);
+// console.log("c =",c,"d =",d,"c -= d", c -=d);
+// console.log("c =",c,"d =",d,"c *= d", c *=d);
+// console.log("c =",c,"d =",d,"c /= d", c /=d);
+// console.log("c =",c,"d =",d,"c **= d", c **=d);
+// console.log("c =",c,"d =",d,"c %= d", c %=d);
+
+// Comparison Operators
+
+// let e = 20;
+// let f = 10;
+// console.log("Comparison Operators")
+// console.log("e =",e,"f =",f);
+
+// console.log("e == f", e == f);
+// console.log("e === f", e === f);
+// console.log("e != f", e != f);
+// console.log("e !== f", e !== f);
+// console.log("e < f", e < f);
+// console.log("e <= f", e <= f);
+// console.log("e > f", e > f);
+// console.log("e >= f", e >= f);
+
+let g = 5;
+let h = 10;
+console.log("Logical Operators")
+console.log("g =",g,"h = ",h);
+console.log("con1 || con2 = ", g < h || h < g);
+console.log("con1 && con2 = ", g < h || h < g);
+console.log("con1 ! con2 = ", ! (g < h));
+
+// ternary Operators
+
+// condition ? true output : false output
+g > h ? console.log("T") : console.log("F");
+
